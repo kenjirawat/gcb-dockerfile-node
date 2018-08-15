252c558fb7ad9287d971e1fca2813f469461e87b
JPaiboon <admin@MacBook-Pro.local>
2018-08-15 10:15:16 +0700

    add send

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
  
 app.get('/', function (req, res) {
     res.send('Hello World!')
+    res.send('Test add say hi')
 })
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
